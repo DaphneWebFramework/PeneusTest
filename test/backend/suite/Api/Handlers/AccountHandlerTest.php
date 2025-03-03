060c16c12b7c8f33c8b9a6f3aaeca05e9f110c61
eylemugurel <eylemugurel@hotmail.com>
2025-03-03 20:36:11 +0100

    Add AccountHandlerTest

---
+++ test/backend/suite/Api/Handlers/AccountHandlerTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+use \PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\Attributes\CoversClass;
+
+use \Peneus\Api\Handlers\AccountHandler;
+
+use \Peneus\Api\Actions\LogoutAction;
+use \Peneus\Api\Guards\SessionGuard;
+use \TestToolkit\AccessHelper;
+
+#[CoversClass(AccountHandler::class)]
+class AccountHandlerTest extends TestCase
+{
+    #region createAction -------------------------------------------------------
+
+    function testCreateActionWithLogout()
+    {
+        $handler = new AccountHandler;
+
+        $action = AccessHelper::CallMethod($handler, 'createAction', ['logout']);
+
+        $this->assertInstanceOf(LogoutAction::class, $action);
+        $guards = AccessHelper::GetProperty($action, 'guards');
+        $this->assertCount(1, $guards);
+        $this->assertInstanceOf(SessionGuard::class, $guards[0]);
+    }
+
+    public function testCreateActionWithUnknownAction()
+    {
+        $handler = new AccountHandler;
+
+        $action = AccessHelper::CallMethod($handler, 'createAction', ['unknown']);
+
+        $this->assertNull($action);
+    }
+
+    #endregion createAction
+}
