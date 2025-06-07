85fcfc4c26f708a72ebb30fc7b10abef94d98471
eylemugurel <eylemugurel@hotmail.com>
2025-06-07 16:15:58 +0200

    Move TransactionalEmailSender trait out of Actions\Account namespace

--- test/backend/suite/Api/Traits/TransactionalEmailSenderTest.php
+++ test/backend/suite/Api/Traits/TransactionalEmailSenderTest.php
@@ -3,7 +3,7 @@ use \PHPUnit\Framework\TestCase;
 use \PHPUnit\Framework\Attributes\CoversClass;
 use \PHPUnit\Framework\Attributes\DataProviderExternal;
 
-use \Peneus\Api\Actions\Account\Traits\TransactionalEmailSender;
+use \Peneus\Api\Traits\TransactionalEmailSender;
 
 use \Harmonia\Config;
 use \Harmonia\Core\CFile;
