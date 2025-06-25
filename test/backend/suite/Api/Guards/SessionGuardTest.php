385edbc9124dfa4d9adbc89b7114a589fb6f2bb2
eylemugurel <eylemugurel@hotmail.com>
2025-06-25 07:53:33 +0200

    SessionGuardTest: Add coverage for minimum role enforcement

--- test/backend/suite/Api/Guards/SessionGuardTest.php
+++ test/backend/suite/Api/Guards/SessionGuardTest.php
@@ -5,6 +5,7 @@ use \PHPUnit\Framework\Attributes\CoversClass;
 use \Peneus\Api\Guards\SessionGuard;
 
 use \Peneus\Model\Account;
+use \Peneus\Model\Role;
 use \Peneus\Services\AccountService;
 
 #[CoversClass(SessionGuard::class)]
@@ -47,6 +48,66 @@ class SessionGuardTest extends TestCase
             ->willReturn(null);
 
         $this->assertFalse($sessionGuard->Verify());
+    }
+
+    function testVerifyWhenAccountRoleMissingAndMinimumRoleSet()
+    {
+        $accountService = AccountService::Instance();
+        $sessionGuard = new SessionGuard(Role::Editor);
+
+        $accountService->expects($this->once())
+            ->method('LoggedInAccount')
+            ->willReturn($this->createStub(Account::class));
+        $accountService->expects($this->once())
+            ->method('LoggedInAccountRole')
+            ->willReturn(null); // No role assigned
+
+        $this->assertFalse($sessionGuard->Verify());
+    }
+
+    function testVerifyWhenAccountRoleBelowMinimum()
+    {
+        $accountService = AccountService::Instance();
+        $sessionGuard = new SessionGuard(Role::Editor);
+
+        $accountService->expects($this->once())
+            ->method('LoggedInAccount')
+            ->willReturn($this->createStub(Account::class));
+        $accountService->expects($this->once())
+            ->method('LoggedInAccountRole')
+            ->willReturn(Role::None); // None < Editor
+
+        $this->assertFalse($sessionGuard->Verify());
+    }
+
+    function testVerifyWhenAccountRoleEqualsMinimum()
+    {
+        $accountService = AccountService::Instance();
+        $sessionGuard = new SessionGuard(Role::Editor);
+
+        $accountService->expects($this->once())
+            ->method('LoggedInAccount')
+            ->willReturn($this->createStub(Account::class));
+        $accountService->expects($this->once())
+            ->method('LoggedInAccountRole')
+            ->willReturn(Role::Editor); // Editor == Editor
+
+        $this->assertTrue($sessionGuard->Verify());
+    }
+
+    function testVerifyWhenAccountRoleExceedsMinimum()
+    {
+        $accountService = AccountService::Instance();
+        $sessionGuard = new SessionGuard(Role::Editor);
+
+        $accountService->expects($this->once())
+            ->method('LoggedInAccount')
+            ->willReturn($this->createStub(Account::class));
+        $accountService->expects($this->once())
+            ->method('LoggedInAccountRole')
+            ->willReturn(Role::Admin); // Admin >= Editor
+
+        $this->assertTrue($sessionGuard->Verify());
     }
 
     #endregion Verify
